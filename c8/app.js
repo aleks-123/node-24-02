4728956a5198ff4d4d773e0b8c8383fc003aed4f
Alex <ace.ki@hotmail.com>
2024-10-01 21:53:43 +0200

    domasna c8

--- c8/app.js
+++ c8/app.js
@@ -6,6 +6,8 @@ const app = express();
 app.get('/bmi/:weight/:height', calculator.bmiCalculator);
 app.get('/newton/:mass/:acc', calculator.calculateForce);
 
+//probajte da kreirate so post metoda istive metodi bmiCalculator i calculatorForce
+
 app.listen(10000, (err) => {
   if (err) return console.log('This server could not start');
   console.log('Server started on port 10000');
